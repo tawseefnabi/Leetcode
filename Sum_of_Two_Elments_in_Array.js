edc539f11d37f7c493bcad2b98ccbd1a55a2eca9
Tawseef Bhat <tawseef.bhat@synapse.com>
2021-03-07 11:59:39 +0530

    sum of two elements equal to target in an array

---
+++ Sum_of_Two_Elments_in_Array.js
@@ -0,0 +1,41 @@
+/**
+ *Given an array of integers nums and an integer target, 
+ *return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, 
+ *and you may not use the same element twice.
+ *You can return the answer in any order.
+
+ */
+
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+ var twoSum = (nums, target) =>{
+  for(let i=0;i<nums.length; i++){
+      for(let j=i+1;j<nums.length;j++){
+          if(nums[i]+nums[j] === target){
+              return [i,j]
+          }
+      }
+  }
+};
+console.time('twoSum runtime in ms: ')
+console.log("twoSum index:",twoSum([2,3,4,5,10,1], 15))
+console.timeEnd('twoSum runtime in ms: ')
+
+var twoSum2 = (nums, target) =>{
+  let map = new Map()
+  for (let i = 0; i < nums.length; i++) {
+    const another = target - nums[i];
+    if (another in map) {
+      return [map[another], i];
+    }
+    map[nums[i]] = i;
+  }
+};
+console.time('twoSum2 runtime in ms: ')
+console.log("twoSum2 index:",twoSum2([2,3,4,5,10,1], 15))
+console.timeEnd('twoSum2 runtime in ms: ')
